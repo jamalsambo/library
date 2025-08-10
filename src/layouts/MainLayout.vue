2d4b50480f3a16b9017a30a7088e02ad3911a02e
Jamal Sambo <jhsambo99@gmail.com>
2025-08-10 19:07:03 +0200

    edicao de footer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -58,9 +58,56 @@
       <router-view />
     </q-page-container>
 
-    <q-footer class="text-center">
-      <div class="q-pa-sm">© Biblioteca Virtual - Template</div>
-    </q-footer>
+    <footer class="bg-primary">
+     <div class="bg-primary text-white">
+    <q-toolbar>
+      <div class="row full-width q-pa-md">
+        <div class="col-12 col-md-4 q-pa-md">
+          <div class="text-h6 q-mb-md">Contatos</div>
+          <div class="q-mb-sm">
+            <q-icon name="phone" size="1rem" class="q-mr-sm" />
+            <span
+              > 84 026 6518  /
+              82 56 68 060  /
+              21420247</span
+            >
+          </div>
+          <div class="q-mb-sm">
+            <q-icon name="email" size="1rem" class="q-mr-sm" />
+            <span>catmoz@geral.com</span>
+          </div>
+        </div>
+        <div class="col-12 col-md-4 q-pa-md">
+          <div class="text-h6 q-mb-md">Localização</div>
+          <div class="q-mb-sm">
+            <q-icon name="place" size="1rem" class="q-mr-sm" />
+            <span>{{ institution?.address }}</span>
+          </div>
+          <div>
+            <q-icon name="map" size="1rem" class="q-mr-sm" />
+            <span
+              >{{ institution?.neighborhood }},
+              {{ institution?.district?.name }}</span
+            >
+          </div>
+        </div>
+        <div class="col-12 col-md-4 q-pa-md text-white">
+          <div class="text-h6 q-mb-md">Redes Sociais</div>
+          <div class="row q-gutter-sm">
+            <q-btn round flat icon="fab fa-facebook" />
+            <q-btn round flat icon="fab fa-instagram" />
+            <q-btn round flat icon="fab fa-twitter" />
+            <q-btn round flat icon="fab fa-youtube" />
+            <q-btn round flat icon="fab fa-linkedin" />
+          </div>
+        </div>
+      </div>
+    </q-toolbar>
+    <div class="text-center q-pa-sm bg-primary">
+      © 2025. Instituto Medio Tecnico Profissional
+    </div>
+  </div>
+    </footer>
   </q-layout>
 </template>
 
